226fd213a3b336672442651aaed946e697f6d987
Joshua-Emakpor <76494935+Joshua-Emakpor@users.noreply.github.com>
2021-07-12 23:02:22 +0100

    Add files via upload
    just added some files

---
+++ new.js
@@ -0,0 +1,13 @@
+var mode = document.querySelector(".dropdown-item1")
+var mode2 = document.querySelector('.dropdown-item2')
+var body = document.querySelector(".oga")
+var dropdown = document.querySelector(".dropdown-menu")
+
+mode.addEventListener("click", () => {
+    body.style.backgroundColor = 'white'
+})
+
+mode2.addEventListener("click", () => {
+    body.style.backgroundColor = 'rgb(48, 53, 51'
+})
+
